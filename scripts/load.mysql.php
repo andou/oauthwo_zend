2a2a7f32847d8694b3bd66494bd3222e2c985b0e
andou <antonio.pastorino@gmail.com>
2012-09-13 15:45:41 +0200

    Commenting the db loading script

--- scripts/load.mysql.php
+++ scripts/load.mysql.php
@@ -1,25 +1,40 @@
 <?php
+/**
+ * 
+ * FactoryLocator.php, 
+ * 
+ * @author framework.zend.com
+ * @version 0.1
+ * 
+ */
 
 /**
-* Script for creating and loading database
-*/
- 
+ * Script for creating and loading database
+ * 
+ * will load schema.mysql.sql, creating the DB schema
+ * with --withdata flag will also load data.mysql.sql, populating the DB
+ * 
+ * usage:
+ * php scripts/load.sqlite.php [--withdata]
+ * 
+ * 
+ */
 // Initialize the application path and autoloading
 defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+        || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
 set_include_path(implode(PATH_SEPARATOR, array(
-    APPLICATION_PATH . '/../library',
-    get_include_path(),
-)));
+            APPLICATION_PATH . '/../library',
+            get_include_path(),
+        )));
 require_once 'Zend/Loader/Autoloader.php';
 Zend_Loader_Autoloader::getInstance();
- 
+
 // Define some CLI options
 $getopt = new Zend_Console_Getopt(array(
-    'withdata|w' => 'Load database with sample data',
-    'env|e-s'    => 'Application environment for which to create database (defaults to development)',
-    'help|h'     => 'Help -- usage message',
-));
+            'withdata|w' => 'Load database with sample data',
+            'env|e-s' => 'Application environment for which to create database (defaults to development)',
+            'help|h' => 'Help -- usage message',
+        ));
 try {
     $getopt->parse();
 } catch (Zend_Console_Getopt_Exception $e) {
@@ -27,46 +42,47 @@ try {
     echo $e->getUsageMessage();
     return false;
 }
- 
+
 // If help requested, report usage message
 if ($getopt->getOption('h')) {
     echo $getopt->getUsageMessage();
     return true;
 }
- 
+
 // Initialize values based on presence or absence of CLI options
 $withData = $getopt->getOption('w');
-$env      = $getopt->getOption('e');
+$env = $getopt->getOption('e');
 defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (null === $env) ? 'development' : $env);
- 
+        || define('APPLICATION_ENV', (null === $env) ? 'development' : $env);
+
 // Initialize Zend_Application
 $application = new Zend_Application(
-    APPLICATION_ENV,
-    APPLICATION_PATH . '/configs/application.ini'
+                APPLICATION_ENV,
+                APPLICATION_PATH . '/configs/application.ini'
 );
- 
+
 // Initialize and retrieve DB resource
 $bootstrap = $application->getBootstrap();
 $bootstrap->bootstrap('db');
 $dbAdapter = $bootstrap->getResource('db');
- 
+
 // let the user know whats going on (we are actually creating a
 // database here)
 if ('testing' != APPLICATION_ENV) {
     echo 'Writing Database in (control-c to cancel): ' . PHP_EOL;
     for ($x = 5; $x > 0; $x--) {
-        echo $x . "\r"; sleep(1);
+        echo $x . "\r";
+        sleep(1);
     }
 }
- 
+
 // Check to see if we have a database file already
 $options = $bootstrap->getOption('resources');
-$dbFile  = $options['db']['params']['dbname'];
+$dbFile = $options['db']['params']['dbname'];
 if (file_exists($dbFile)) {
     unlink($dbFile);
 }
- 
+
 // this block executes the actual statements that were loaded from
 // the schema file.
 try {
@@ -74,13 +90,13 @@ try {
     // use the connection directly to load sql in batches
     $dbAdapter->getConnection()->exec($schemaSql);
     //chmod($dbFile, 0666);
- 
+
     if ('testing' != APPLICATION_ENV) {
         echo PHP_EOL;
         echo 'Database Created';
         echo PHP_EOL;
     }
- 
+
     if ($withData) {
         $dataSql = file_get_contents(dirname(__FILE__) . '/data.mysql.sql');
         // use the connection directly to load sql in batches
@@ -90,12 +106,11 @@ try {
             echo PHP_EOL;
         }
     }
- 
 } catch (Exception $e) {
     echo 'AN ERROR HAS OCCURED:' . PHP_EOL;
     echo $e->getMessage() . PHP_EOL;
     return false;
 }
- 
+
 // generally speaking, this script will be run from the command line
 return true;
